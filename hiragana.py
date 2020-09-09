9cac34fe847b832fa0d45310aa0c46573fc61dc8
Luke Brandon <lukebrandon68@gmail.com>
2020-09-09 10:14:00 -0500

    added to show the answer as well

--- hiragana.py
+++ hiragana.py
@@ -3,9 +3,21 @@ import random
 # The indexes of all of these arrays should always line up
 controller = [0,0,0,0,0]
 controllerNames=["Lesson 1", "Lesson 2", "Lesson 3", "Lesson 4", "Lesson 5"]
-values = [["a", "i", "u", "e", "o", "ka", "ki", "ku", "ke", "ko","sa", "shi", "su", "se", "so"], ["ta", "chi", "tsu", "te", "to", "na", "ni", "nu", "ne", "no", "ha", "hi", "fu", "he", "ho"],
-            ["ma", "mi", "mu", "me", "mo", "ya", "yu", "yo", "ra", "ri", "ru", "re", "ro", "wa", "o(weird)", "n"], ["ga", "gi", "gu", "ge", "go", "za", "ji", "zu", "ze", "zo"], 
-            ["da", "dji", "dzu", "de", "do", "ba", "bi", "bu", "be", "bo", "pa", "pi", "pu", "pe", "po"]]
+values = [[["a", "あ"], ["i", "い"], ["u", "う"], ["e", "え"], ["o", "お"], 
+                            ["ka", "か"], ["ki", "き"], ["ku", "く"], ["ke", "け"], ["ko", "こ"], 
+                            ["sa", "さ"], ["shi", "し"], ["su", "す"], ["se", "せ"], ["so", "そ"]], 
+        [["ta", "た"], ["chi", "ち"], ["tsu", "つ"], ["te", "て"], ["to", "と"],
+                             ["na", "な"], ["ni", "に"], ["nu", "ぬ"], ["ne", "ね"], ["no", "の"], 
+                             ["ha", "は"], ["hi", "ひ"], ["fu", "ふ"], ["he", "へ"], ["ho", "ほ"]],
+        [["ma", "ま"], ["mi", "み"], ["mu", "む"], ["me", "め"], ["mo", "も"], 
+                            ["ya", "や"], ["yu", "ゆ"], ["yo", "よ"], 
+                            ["ra", "ら"], ["ri", "り"], ["ru", "る"], ["re", "れ"], ["ro", "ろ"], 
+                            ["wa", "わ"], ["o(weird)",""], ["n", "ｎ"]], 
+        [["ga", "が"], ["gi", "ぎ"], ["gu", "ぐ"], ["ge", "げ"], ["go", "ご"], 
+                            ["za", "ざ"], ["ji", "じ"], ["zu", "ず"], ["ze", "ぜ"], ["zo", "ぞ"]], 
+        [["da", "だ"], ["dji","ぢ"], ["dzu", "づ"], ["de", "で"], ["do", "ど"], \
+                            ["ba", "ば"], ["bi", "び"], ["bu", "ぶ"], ["be", "べ"], ["bo", "ぼ"], 
+                            ["pa", "ぱ"], ["pi", "ぴ"], ["pu", "ぷ"], ["pe", "ぺ"], ["po", "ぽ"]]]
 
 # TODO: Change these to be organized by Lesson 1,2,3,etc. instead of by their leading character
 
@@ -45,7 +57,8 @@ def beginTesting():
 
     # Print out the characters one by one
     for i in range((numSetsTesting*5)):
-        input(combinedArray[i])
+        input(combinedArray[i][0])
+        input(combinedArray[i][1])
 
 print("Get ready to practice some Hiragana!!")
 whichCharacters()
