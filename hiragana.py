37fa0df4ed19ef14937c92ca5af15754a948a39a
LukeBrandon <lukebrandon68@gmail.com>
2020-09-17 10:48:03 -0500

    fixed incorrect looping and added Katakana

--- hiragana.py
+++ hiragana.py
@@ -3,21 +3,21 @@ import random
 # The indexes of all of these arrays should always line up
 controller = [0,0,0,0,0]
 controllerNames=["Lesson 1", "Lesson 2", "Lesson 3", "Lesson 4", "Lesson 5"]
-values = [[["a", "あ"], ["i", "い"], ["u", "う"], ["e", "え"], ["o", "お"], 
-                            ["ka", "か"], ["ki", "き"], ["ku", "く"], ["ke", "け"], ["ko", "こ"], 
-                            ["sa", "さ"], ["shi", "し"], ["su", "す"], ["se", "せ"], ["so", "そ"]], 
-        [["ta", "た"], ["chi", "ち"], ["tsu", "つ"], ["te", "て"], ["to", "と"],
-                             ["na", "な"], ["ni", "に"], ["nu", "ぬ"], ["ne", "ね"], ["no", "の"], 
-                             ["ha", "は"], ["hi", "ひ"], ["fu", "ふ"], ["he", "へ"], ["ho", "ほ"]],
-        [["ma", "ま"], ["mi", "み"], ["mu", "む"], ["me", "め"], ["mo", "も"], 
-                            ["ya", "や"], ["yu", "ゆ"], ["yo", "よ"], 
-                            ["ra", "ら"], ["ri", "り"], ["ru", "る"], ["re", "れ"], ["ro", "ろ"], 
-                            ["wa", "わ"], ["o(weird)",""], ["n", "ｎ"]], 
-        [["ga", "が"], ["gi", "ぎ"], ["gu", "ぐ"], ["ge", "げ"], ["go", "ご"], 
-                            ["za", "ざ"], ["ji", "じ"], ["zu", "ず"], ["ze", "ぜ"], ["zo", "ぞ"]], 
-        [["da", "だ"], ["dji","ぢ"], ["dzu", "づ"], ["de", "で"], ["do", "ど"],
-                            ["ba", "ば"], ["bi", "び"], ["bu", "ぶ"], ["be", "べ"], ["bo", "ぼ"], 
-                            ["pa", "ぱ"], ["pi", "ぴ"], ["pu", "ぷ"], ["pe", "ぺ"], ["po", "ぽ"]]]
+values = [[["a", "あ ア"], ["i", "い イ"], ["u", "う ウ"], ["e", "え エ"], ["o", "お オ"], 
+                            ["ka", "か カ"], ["ki", "き キ"], ["ku", "く ク"], ["ke", "け ケ"], ["ko", "こ コ"], 
+                            ["sa", "さ サ"], ["shi", "し シ"], ["su", "す ス"], ["se", "せ セ"], ["so", "そ ソ"]], 
+        [["ta", "た タ"], ["chi", "ち チ"], ["tsu", "つ ツ"], ["te", "て テ"], ["to", "と ト"],
+                             ["na", "な ナ"], ["ni", "に ニ"], ["nu", "ぬ ヌ"], ["ne", "ね ネ"], ["no", "の ノ"], 
+                             ["ha", "は ハ"], ["hi", "ひ ヒ"], ["fu", "ふ フ"], ["he", "へ ヘ"], ["ho", "ほ ホ"]],
+        [["ma", "ま マ"], ["mi", "み ミ"], ["mu", "む ム"], ["me", "め メ"], ["mo", "も モ"], 
+                            ["ya", "や ヤ"], ["yu", "ゆ ユ"], ["yo", "よ ヨ"], 
+                            ["ra", "ら ラ"], ["ri", "り リ"], ["ru", "る ル"], ["re", "れ レ"], ["ro", "ろ ロ"], 
+                            ["wa", "わ ワ"], ["o(weird)",""], ["n", "ん ン"]], 
+        [["ga", "が ガ"], ["gi", "ぎ ギ"], ["gu", "ぐ グ"], ["ge", "げ　ゲ"], ["go", "ご　ゴ"], 
+                            ["za", "ざ　ザ"], ["ji", "じ　ジ"], ["zu", "ず　ズ"], ["ze", "ぜ　ゼ"], ["zo", "ぞ　ゾ"]], 
+        [["da", "だ　ダ"], ["dji","ぢ"], ["dzu", "づ"], ["de", "で　デ"], ["do", "ど　ド"],
+                            ["ba", "ば　バ"], ["bi", "び　ビ"], ["bu", "ぶ　ブ"], ["be", "べ　ベ"], ["bo", "ぼ　ボ"], 
+                            ["pa", "ぱ　パ"], ["pi", "ぴ　ピ"], ["pu", "ぷ　プ"], ["pe", "ぺ　ペ"], ["po", "ぽ ポ"]]]
 
 # TODO: Change these to be organized by Lesson 1,2,3,etc. instead of by their leading character
 
@@ -56,9 +56,11 @@ def beginTesting():
     random.shuffle(combinedArray)
 
     # Print out the characters one by one
-    for i in range((numSetsTesting*5)):
+    i = 0
+    while i< len(combinedArray):
         input(combinedArray[i][0])
         input(combinedArray[i][1])
+        i += 1
 
 print("Get ready to practice some Hiragana!!")
 whichCharacters()
